6ea1cb70451f89aaf8fa07ea1ea90fc9615588ef
ochukoemmanuel <ochukoemmanuel07@gmail.com>
2021-11-22 10:45:27 +0100

    Created hero section, cards section and route to services page, product page, sign-up page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
-    <div>
       <Router>
+      <div>
         <Navbar />
+
         <Routes>
-          <Route
-            path="/"
-            elements={
-              <div>
-                <Navbar />
-              </div>
-            }
-          />
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
-      </Router>
     </div>
+      </Router>
   );
 }
 
